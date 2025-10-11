b95c361ef6fb2fa9bc878491d7b5efcd88c2038b
Adishree <adishree@argusoft.com>
2025-10-11 10:11:50 +0530

    Hapi-Fhir Testing

---
+++ src/main/java/com/example/demo/PatientController.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import org.hl7.fhir.r4.model.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("api/patient")
+public class PatientController {
+
+    @GetMapping("/describe")
+    public String describePatient(){
+        Patient patient = new Patient();
+//        CodeableConcept maritalStatus = new CodeableConcept();
+
+//        patient.setMaritalStatus()
+        patient.addName(new HumanName().setFamily("Shree")
+                .addGiven("Adishree")
+                .addGiven("Adi"));
+        patient.setGender(Enumerations.AdministrativeGender.FEMALE);
+        patient.setBirthDate(new Date(2004,3,28));
+        patient.addTelecom(new ContactPoint().setSystem(ContactPoint.ContactPointSystem.PHONE));
+        patient.addAddress(new Address()
+                .setCity("Cuttack")
+                .setState("Odisha")
+                );
+        FhirContext ctx = FhirContext.forR4();
+        String FhirJSON = ctx.newJsonParser()
+                .setPrettyPrint(true)
+                .encodeResourceToString(patient);
+        System.out.println(FhirJSON);
+
+        IParser parser = ctx.newJsonParser();
+        Patient patient1= parser.parseResource(Patient.class,FhirJSON);
+        String patientId = patient1.getName().getFirst().getFamily();
+        String gender = patient1.getGender().toCode();
+
+        System.out.println("Patient ID: " + patientId);
+        System.out.println("Gender: " + gender);
+        return FhirJSON;
+    }
+}
---
+++ src/main/java/com/example/demo/PhrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrApplication.class, args);
+	}
+
+}
