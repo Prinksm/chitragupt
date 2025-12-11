7f8b660bcb16cbcda95ea6cf6d98adcda260a702
Prinksm <prinkal.mahajan@argusoft.com>
2025-12-11 12:09:19 +0530

    mail fixed

--- src/main/java/com/example/demo/emails/services/EmailService.java
+++ src/main/java/com/example/demo/emails/services/EmailService.java
@@ -1,15 +1,16 @@
 package com.example.demo.emails.services;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
+// import jakarta.mail.MessagingException;
+// import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
+// // import org.springframework.mail.javamail.MimeMessageHelper;
+// import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
     // private JavaMailSender mailSender;
     private final ResendEmailClient resendEmailClient;
