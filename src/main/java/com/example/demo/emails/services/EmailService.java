251257ac95ae01e6afacfde01c1720a41d70b79a
Prinksm <prinkal.mahajan@argusoft.com>
2025-12-11 12:54:11 +0530

    Fix; Import Errors

--- src/main/java/com/example/demo/emails/services/EmailService.java
+++ src/main/java/com/example/demo/emails/services/EmailService.java
@@ -4,6 +4,9 @@ package com.example.demo.emails.services;
 // import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 // import org.springframework.mail.SimpleMailMessage;
 // import org.springframework.mail.javamail.JavaMailSender;
 // // import org.springframework.mail.javamail.MimeMessageHelper;
--- src/main/java/com/example/demo/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/error/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
