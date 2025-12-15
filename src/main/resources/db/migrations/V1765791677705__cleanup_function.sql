d53ffcd3172b91e3b28227ac590bb4c59ab81a52
Prinksm <prinkal.mahajan@argusoft.com>
2025-12-15 16:11:30 +0530

    migration fixed

--- src/main/resources/db/migrations/V1765791677705__cleanup_function.sql
+++ src/main/resources/db/migrations/V1765791677705__cleanup_function.sql
@@ -2,20 +2,20 @@ CREATE OR REPLACE FUNCTION cleanup_app_data()
 RETURNS void AS $$
 BEGIN
     -- Delete expired OTPs
-    DELETE FROM otp
+    DELETE FROM users.otp
     WHERE expires_at < now();
 
     -- Delete unverified users older than 10 minutes
-    DELETE FROM users
+    DELETE FROM users.users
     WHERE verified = false
     AND created_at < now() - interval '10 minutes';
 
     -- Delete expired password reset tokens
-    DELETE FROM password_reset_token
+    DELETE FROM users.password_reset_token
     WHERE expiry_date < now();
 
     -- Delete patients without users
-    DELETE FROM patient p
+    DELETE FROM patient.patient p
     WHERE NOT EXISTS (
         SELECT 1 FROM users u WHERE u.id = p.user_id
     );
